8d49a0cd44234be78832d52cd1d534a4f06c44c9
Stephen Tse <xjiajiahao@gmail.com>
2017-09-05 10:03:47 +0800

    fixed grdient again

--- ObjFunc.m
+++ ObjFunc.m
@@ -24,7 +24,7 @@ classdef ObjFunc
         % compute gradient without regularizer
         function gradient = Gradient(~, w, X, y))
             tmpExp = exp(-y .* (w'*X)')'; % 1-by-n vector
-            gradient = mean((-y' .* tmpExp' .* X ) ./ (1 + tmpExp), 2);
+            gradient = mean((-y' .* tmpExp .* X ) ./ (1 + tmpExp), 2);
         end
 
         % compute hypothesis
