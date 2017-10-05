0f4409eead30686240deb160ec06e78b7cf9b0c6
Stephen Tse <xjiajiahao@gmail.com>
2017-10-05 22:00:43 +0800

    we are now able to draw 9 plots

--- PlotCurve.m
+++ PlotCurve.m
@@ -1,6 +1,6 @@
 function PlotCurve(data_point, curve_style, label, name, gridNum)
 
-subplot(2, 3, gridNum)
+subplot(3, 3, gridNum)
 % subplot(1, 1, gridNum)
 title(name)
 semilogy(data_point(:, 1), data_point(:, 3), curve_style, 'linewidth', 2, 'DisplayName', label);
@@ -12,7 +12,7 @@ legend('show')
 hold on
 
 
-subplot(2, 3, gridNum + 3)
+subplot(3, 3, gridNum + 3)
 semilogy(data_point(:, 1), data_point(:, 4), curve_style, 'linewidth', 2, 'DisplayName', label);
 xlabel('epoch')
 ylabel('distance to opt')
@@ -20,3 +20,13 @@ legend('show')
 % box on
 % grid on
 hold on
+
+subplot(3, 3, gridNum+6)
+title(name)
+% semilogy(data_point(:, 2), data_point(:, 3), curve_style, 'linewidth', 2, 'DisplayName', label);
+semilogy(data_point(:, 2), data_point(:, 4), curve_style, 'linewidth', 2, 'DisplayName', label);
+hold on;
+xlabel('time(seconds)')
+% ylabel('log-suboptimality')
+ylabel('distance to opt')
+legend('show')
