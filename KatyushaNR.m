1a16941ca028c4c8409de5281232cee9637e217f
Stephen Tse <xjiajiahao@gmail.com>
2017-10-06 09:57:19 +0800

    DVRG-K now supports minibatch

--- KatyushaNR.m
+++ KatyushaNR.m
@@ -1,17 +1,22 @@
-function wOpt = KatyushaNR(objFunc, X, y, Xtest, ytest, passes, factor, dataset, gridNum)
+function subOptimality = KatyushaNR(objFunc, X, y, Xtest, ytest, passes, factor, batchSize, dataset, gridNum)
 
-fprintf('Fitting data with KatyushaNR...\n');
+fprintf('Fitting data with DVRG-K ...\n');
 
 % initialization
-[d ,n] = size(X);
-iterNum = n;
+[d, n] = size(X);
+iterNum = floor(n/batchSize);
+done = iterNum*batchSize;  % this variable tells us whether we need to do the last iteration
 lambda = objFunc.lambda;
 
 eta = factor / objFunc.L
 % eta = 5e-1
 
-wtilde = zeros(d, 1);
-
+if issparse(X)
+    wtilde = sparse(d, 1);
+else
+    wtilde = zeros(d, 1);
+end
+w = wtilde;
 
 initCost = objFunc.PrintCost(wtilde, X, y, 0);
 subOptimality = [0, 0, 1, 1];
@@ -29,16 +34,16 @@ for s = 1:passes % for each epoch
     ntilde = objFunc.Gradient(wtilde, X, y);
 
     for i = 1:iterNum
-        idx = mod(i-1, n) + 1;
-        % idx = randperm(n, 1);
+        idx = (i-1)*batchSize + 1 : i*batchSize;
+
         w = tau1 * z + tau2 * wtilde + (1 - tau2 - tau1) * u;
 
         Xtmp = X(:, idx);
         ytmp = y(idx);
         % new gradient
-        tmpExp = exp(-ytmp .* (w'*Xtmp)')'; % 1-by-n vector
+        tmpExp = exp(-ytmp .* (Xtmp' *w))'; % 1-by-n vector
         % old gradient
-        tmpExpTilde = exp(-ytmp .* (wtilde'*Xtmp)')'; % 1-by-n vector
+        tmpExpTilde = exp(-ytmp .* (Xtmp' * wtilde))'; % 1-by-n vector
         wDelta1 = mean(-ytmp' .* (1./(1 + tmpExpTilde) - 1./(1 + tmpExp)) .* Xtmp, 2);
 
         wDelta2 = wDelta1 + lambda * w;
@@ -48,8 +53,28 @@ for s = 1:passes % for each epoch
         u = w + tau1 * (znew - z);
         z = znew;
     end
-    wtilde = u;
 
+    if done < n
+        idx = done + 1 : n;
+        w = tau1 * z + tau2 * wtilde + (1 - tau2 - tau1) * u;
+
+        Xtmp = X(:, idx);
+        ytmp = y(idx);
+        % new gradient
+        tmpExp = exp(-ytmp .* (Xtmp' *w))'; % 1-by-n vector
+        % old gradient
+        tmpExpTilde = exp(-ytmp .* (Xtmp' * wtilde))'; % 1-by-n vector
+        wDelta1 = mean(-ytmp' .* (1./(1 + tmpExpTilde) - 1./(1 + tmpExp)) .* Xtmp, 2);
+
+        wDelta2 = wDelta1 + lambda * w;
+        wDelta = wDelta2 + ntilde;
+
+        znew = z - alpha * wDelta;
+        u = w + tau1 * (znew - z);
+        z = znew;
+    end
+
+    wtilde = u;
 
     % print and plot
     cost = objFunc.PrintCost(wtilde, X, y, s);
@@ -72,6 +97,8 @@ fprintf('time elapsed: %f\n', telapsed);
 
 label = 'DVRG-K';
 curve_style = 'b-';
+
+% PlotTime(subOptimality, curve_style, label, dataset, gridNum);
 PlotCurve(subOptimality, curve_style, label, dataset, gridNum);
 
 end  % function
