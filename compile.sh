c9ebf26b3a3db324afaece9ca0c324daf6ee3d8d
Stephen Tse <xjiajiahao@gmail.com>
2017-10-30 11:00:06 +0800

    doing experiments on criteo

--- compile.sh
+++ compile.sh
@@ -6,7 +6,7 @@ mex -largeArrayDims C/IAG_logistic.c ${MKLROOT}/lib/intel64/libmkl_intel_ilp64.a
 
 mex -largeArrayDims C/DIG_logistic.c ${MKLROOT}/lib/intel64/libmkl_intel_ilp64.a ${MKLROOT}/lib/intel64/libmkl_intel_thread.a ${MKLROOT}/lib/intel64/libmkl_core.a -liomp5 -lpthread -lm -ldl
 
-mex -largeArrayDims C/Katyusha_logistic.c ${MKLROOT}/lib/intel64/libmkl_intel_ilp64.a ${MKLROOT}/lib/intel64/libmkl_intel_thread.a ${MKLROOT}/lib/intel64/libmkl_core.a -liomp5 -lpthread -lm -ldl
+# mex -largeArrayDims C/Katyusha_logistic.c ${MKLROOT}/lib/intel64/libmkl_intel_ilp64.a ${MKLROOT}/lib/intel64/libmkl_intel_thread.a ${MKLROOT}/lib/intel64/libmkl_core.a -liomp5 -lpthread -lm -ldl
 
 mex -largeArrayDims C/Katyusha_sparse.c ${MKLROOT}/lib/intel64/libmkl_intel_ilp64.a ${MKLROOT}/lib/intel64/libmkl_intel_thread.a ${MKLROOT}/lib/intel64/libmkl_core.a -liomp5 -lpthread -lm -ldl
 
--- criteo_main.m
+++ criteo_main.m
@@ -3,114 +3,19 @@ dataset = 'criteo';
 gridNum = 3;
 % dataset : toy, covtype, rcv1, avazu, MNIST. HIGGS
 
-if strcmp(dataset, 'covtype')
-    passes = 20;
-    factor = 1/2;
-    factorNR = 1/2;
-    alpha = 1;
-    factorIAG = 1e-5;
-    lambda = 1e-5;
-    batchSize = 1;
+passes = 20;
+factor = 1/2;
+factorNR = 1/2;
+% factorA = 0.1;
+factorIAG = 0.01;
+% factorGD = 10;
+alphaNR = 1;
+alpha = 1;
+factorDIG = 1;
+lambda = 1e-9;
+batchSize = 1;
+ourlimit = 5000;
 
-% elseif strcmp(dataset, 'rcv1')
-%     passes = 20;
-%     factor = 3/8;
-%     factorA = 3/8;
-%     factorNR = 3/8;
-%     alpha = 1;
-%     lambda = 1e-5;
-%     batchSize = 1;
-
-% elseif strcmp(dataset, 'rcv1')
-%     passes = 20;
-%     factor = 3/8;
-%     factorA = 1/2;
-%     factorNR = 0.6875;
-%     alpha = 1.5;
-%     lambda = 1e-5;
-%     batchSize = 1;
-
-% elseif strcmp(dataset, 'rcv1')
-%     passes = 20;
-%     factor = 1;
-%     factorA = 1;
-%     factorIAG = 0.05;
-%     factorNR = 3/8;
-%     alpha = 1;
-%     lambda = 1e-8;
-%     batchSize = 1;
-
-elseif strcmp(dataset, 'rcv1')
-    passes = 20;
-    factor = 1;
-    factorA = 0.5;
-    factorIAG = 0.1;
-    factorNR = 1;
-    alpha = 1;
-    lambda = 1e-8;
-    batchSize = 1;
-    ourlimit = 5000;
-elseif strcmp(dataset, 'MNIST')
-    passes = 20;
-    factor = 0.2;
-    factorNR = 0.2;
-    factorA = 0.1;
-    factorIAG = 0.01;
-    factorGD = 10;
-    alphaNR = 1;
-    alpha = 1;
-    factorDIG = 1;
-    lambda = 1e-4;
-    batchSize = 1;
-    ourlimit = 5000;
-
-% elseif strcmp(dataset, 'avazu')
-%     passes = 20;
-%     factor = 1/4;
-%     factorIAG = 0.05;
-%     alpha = 1;
-%     lambda = 1e-5;
-%     batchSize = 64;
-
-% elseif strcmp(dataset, 'avazu')
-%     passes = 10;
-%     factor = 10;
-%     factorNR = 10;
-%     factorIAG = 0.05;
-%     factorDIG = 1;
-%     alpha = 9;
-%     alphaNR = 9;
-%     lambda = 1e-8;
-%     batchSize = 64;
-%     ourlimit = 5000;
-
-elseif strcmp(dataset, 'avazu')
-    passes = 1;
-    factor = 1;
-    factorNR = 10;
-    factorIAG = 5e-6;
-    factorDIG = 1;
-    alpha = 9;
-    alphaNR = 9;
-    lambda = 1e-8;
-    batchSize = 1;
-    ourlimit = 5000;
-
-elseif strcmp(dataset, 'criteo')
-    passes = 20;
-    factor = 1/2;
-    alpha = 1;
-    lambda = 1e-9;
-    batchSize = 1;
-    ourlimit = 5000;
-elseif strcmp(dataset, 'HIGGS')
-    passes = 20;
-    factor = 0.1;
-    factorNR = 0.1;
-    alpha = 1;
-    lambda = 1e-5;
-    batchSize = 1;
-end
 %% preliminaries
 % [Xtrain, Xtest, ytrain, ytest] = LoadDataset(dataset);  % load dataset
 
