7e4003d6a0f05b1bc7296cbf5d249c7798859340
Stephen Tse <xjiajiahao@gmail.com>
2017-11-01 09:58:44 +0800

    updated svm_FindOptSolution

---
+++ HIGGS_main.m
@@ -0,0 +1,69 @@
+% function main(dataset, gridNum)
+dataset = 'HIGGS';
+gridNum = 3;
+% dataset : toy, covtype, rcv1, avazu, MNIST.
+lambda = 1e-8;
+passes = 50;
+factor = 0.1;
+factorNR = 0.35;
+alpha = 0.5;
+alphaNR = 0.6;
+factorIAG = 1e-4;
+factorDIG = 1;
+factorA = 0.6;
+facotrGD = 1;
+batchSize = 1;
+ourlimit = 5000;
+
+%% preliminaries
+% [Xtrain, Xtest, ytrain, ytest] = LoadDataset(dataset);  % load dataset
+[d, n] = size(Xtrain);
+Z = -ytrain' .* Xtrain;
+ZT = Z';
+
+% L = max(sum(Xtrain.^2, 1)) * 2 + lambda; % if we did not normalize
+L = 2 * sum(Xtrain(:, 1).^2) + lambda; % if we have normalized
+mu = lambda;
+svmCost = SVM(lambda, L, mu);
+
+% fig = figure('units', 'normalized', 'outerposition', [0 0 1 1]);
+
+% find or load optimal solution
+objFuncType = '_svm';
+filename = strcat('../data/', dataset, objFuncType, '_opt.mat');
+if exist(filename, 'file') ~= 2
+    wOpt = svm_FindOptSolution(svmCost, Xtrain, ytrain, Z, ZT, Xtest, ytest, passes*20, factor, batchSize);
+    save(filename, 'wOpt'); %@TODO
+else
+    load(filename, 'wOpt');
+end
+svmCost.optSolution = wOpt;
+svmCost.optCost = svmCost.Cost(wOpt, ZT)
+
+%% have fun
+
+% filename = strcat('../data/', dataset, '_result_5.mat');
+% load(filename);
+
+subOptNR = svm_SVRGNR(svmCost, Xtrain, ytrain, Z, ZT, Xtest, ytest, passes, factorNR, batchSize, dataset, gridNum, ourlimit);
+subOptK = svm_KatyushaNR(svmCost, Xtrain, ytrain, Z, ZT, Xtest, ytest, passes, alphaNR, batchSize, dataset, gridNum, ourlimit);
+subOptIAG = svm_IAG(svmCost, Xtrain, ytrain, Z, ZT, Xtest, ytest, passes, factorIAG, dataset, gridNum, ourlimit);
+subOpt = svm_SVRG(svmCost, Xtrain, ytrain, Z, ZT, Xtest, ytest, passes, factor, batchSize, dataset, gridNum, ourlimit);
+subOptDIG = svm_DIG(svmCost, Xtrain, ytrain, Z, ZT, Xtest, ytest, passes, factorDIG, dataset, gridNum, ourlimit, ourlimit);
+subOptKatyusha = svm_Katyusha(svmCost, Xtrain, ytrain, Z, ZT, Xtest, ytest, passes, alpha, batchSize, dataset, gridNum, ourlimit);
+
+% subOptRR = svm_SVRGRR(svmCost, Xtrain, ytrain, Z, ZT, Xtest, ytest, passes, factor, batchSize, dataset, gridNum, ourlimit);
+% subOptA = svm_SAGA(svmCost, Xtrain, ytrain, Z, ZT, Xtest, ytest, passes, factorA, dataset, gridNum, ourlimit);
+% subOptGD = svm_GD(svmCost, Xtrain, ytrain, Z, ZT, Xtest, ytest, passes, factor, batchSize, dataset, gridNum, ourlimit);
+
+
+% save(filename, 'subOpt', 'subOptNR', 'subOptK', 'subOptRR', 'subOptA', 'subOptGD');
+
+% Katyusha(svmCost, Xtrain, ytrain, Xtest, ytest, passes, factor);
+% SAGA(svmCost, Xtrain, ytrain, Xtest, ytest, passes, factor);
+% SGD(svmCost, Xtrain, ytrain, Xtest, ytest, passes, factor);
+% GD(svmCost, Xtrain, ytrain, Xtest, ytest, passes, factor);
+%% save figure and exit
+% figname = strcat('./', dataset, objFuncType, '.png');
+% saveas(fig, figname);
+% close(fig);
--- SVM/svm_FindOptSolution.m
+++ SVM/svm_FindOptSolution.m
@@ -12,13 +12,10 @@ lambda = objFunc.lambda;
 eta = factor / objFunc.L
 % eta = 5e-1
 
-if issparse(X)
-    wtilde = sparse(d, 1);
-else
-    wtilde = zeros(d, 1);
-end
-w = wtilde;
-wOpt = wtilde;
+wtilde = zeros(d, 1);
+w = zeros(d, 1);
+wOpt = zeros(d, 1);
+
 optCost = objFunc.PrintCost(wtilde, ZT, 0);;
 preCost = optCost;
 reward = 0;
@@ -27,18 +24,24 @@ reward = 0;
 for s = 1:passes % for each epoch
     ntilde = objFunc.Gradient(wtilde, Z, ZT);
 
-    for i = 1:iterNum
-        idx = randperm(n, batchSize);
-        Ztmp = Z(:, idx);
-        ZTtmp = Ztmp';
+    % for i = 1:iterNum
+    %     idx = randperm(n, batchSize);
+    %     Ztmp = Z(:, idx);
+    %     ZTtmp = Ztmp';
+    %
+    %     tmpDeltaG = Ztmp * (max(1 + ZTtmp * w, 0) - max(1 + ZTtmp * wtilde, 0)) * 2/batchSize;
+    %
+    %     wDelta1 = tmpDeltaG + lambda * w;
+    %     wDelta2 = wDelta1 + ntilde;
+    %     w = w - eta * wDelta2;
+    % end
+    % wtilde = w;
 
-        tmpDeltaG = Ztmp * (max(1 + ZTtmp * w, 0) - max(1 + ZTtmp * wtilde, 0)) * 2/batchSize;
+    ntilde = full(ntilde);
+    iVals = int32(ceil(n*rand(iterNum, 1)));
+    SVRG_svm(w, wtilde, ntilde, Z, lambda, eta, iterNum, iVals);
+    wtilde(:) = w(:); % to avoid copy-on-write
 
-        wDelta1 = tmpDeltaG + lambda * w;
-        wDelta2 = wDelta1 + ntilde;
-        w = w - eta * wDelta2;
-    end
-    wtilde = w;
     currentCost = objFunc.PrintCost(wtilde, ZT, s);
     if currentCost <= optCost
         optCost = currentCost;
